bcc44af3d2b69b0d86412227d1a3fd3014ecd6e0
asad <asaduzzamanprogrammer@gmail.com>
2024-02-08 15:12:37 +0600

    function-start

---
+++ function.js
@@ -0,0 +1,26 @@
+// function borgo(num){
+//     const oho = num*num;
+//     console.log(oho);
+
+// }
+
+// borgo(8);
+// borgo(7)
+
+
+// function jog(num1, num2){
+//     const total = num1 + num2;
+//     console.log(total);
+// }
+
+// jog(65, 5)
+
+function dif(num1, num2){
+    const par = num1 - num2;
+    console.log(par)
+}
+
+const fatherAge = 55;
+const motherAge = 44;
+
+dif(fatherAge, motherAge)
